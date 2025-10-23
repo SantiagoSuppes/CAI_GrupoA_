59be2430eda5c2f2fedb7a2bf9ddde8f39b6fda7
FranuDev <francodiiasi12@gmail.com>
2025-10-22 21:26:08 -0300

    ASASAS

---
+++ CdEntregarCliente/Guia.cs
@@ -0,0 +1,15 @@
+﻿namespace CAI_GrupoA_.AgenciaEntregarCliente
+{
+    internal class Guia
+    {
+        public string NumeroGuia { get; set; }
+        public DateTime FechaImposicion { get; set; }
+
+        // ✅ Usa el enum local, no el de Entidades
+        public TamañoCajaEnum TamañoCaja { get; set; }
+
+        public string Destinatario { get; set; }
+        public string Remitente { get; set; }
+    }
+}
+
